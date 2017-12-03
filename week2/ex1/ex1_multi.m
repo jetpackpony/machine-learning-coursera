c7a465e803d34431235c222da59ae1a3c518b09d
jetpackpony <jetpackpony@gmail.com>
2017-12-03 09:50:23 +0300

    Complete week2 assignments

--- week2/ex1/ex1_multi.m
+++ week2/ex1/ex1_multi.m
@@ -43,12 +43,19 @@ m = length(y);
 fprintf('First 10 examples from the dataset: \n');
 fprintf(' x = [%.0f %.0f], y = %.0f \n', [X(1:10,:) y(1:10,:)]');
 
+figure; % open a new figure window
+
+plot(X(:,1), y, 'rx', 'MarkerSize', 10); % Plot the data
+ylabel('Price'); % Set the y−axis label
+xlabel('House Size'); % Set the x−axis label
+
 fprintf('Program paused. Press enter to continue.\n');
 pause;
 
 % Scale features and set them to zero mean
 fprintf('Normalizing Features ...\n');
 
+X_orig = X;
 [X mu sigma] = featureNormalize(X);
 
 % Add intercept term to X
@@ -85,15 +92,21 @@ fprintf('Running gradient descent ...\n');
 alpha = 0.01;
 num_iters = 400;
 
-% Init Theta and Run Gradient Descent 
+% Init Theta and Run Gradient Descent
 theta = zeros(3, 1);
 [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters);
 
 % Plot the convergence graph
-figure;
-plot(1:numel(J_history), J_history, '-b', 'LineWidth', 2);
-xlabel('Number of iterations');
-ylabel('Cost J');
+%figure;
+%plot(1:numel(J_history), J_history, '-b', 'LineWidth', 2);
+%xlabel('Number of iterations');
+%ylabel('Cost J');
+
+% Plot the linear fit
+hold on; % keep previous plot visible
+plot(X_orig(:,1), X*theta, '-')
+legend('Training data', 'Linear regression')
+hold off % don't overlay any more plots on this figure
 
 % Display gradient descent's result
 fprintf('Theta computed from gradient descent: \n');
@@ -104,7 +117,7 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = 0; % You should change this
+price = [1 ([1650 3] .- mu) ./ sigma] * theta;
 
 
 % ============================================================
@@ -149,7 +162,7 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
+price = [1 1650 3] * theta;
 
 
 % ============================================================
--- week2/ex1/featureNormalize.m
+++ week2/ex1/featureNormalize.m
@@ -6,9 +6,10 @@ function [X_norm, mu, sigma] = featureNormalize(X)
 %   working with learning algorithms.
 
 % You need to set these values correctly
-X_norm = X;
-mu = zeros(1, size(X, 2));
-sigma = zeros(1, size(X, 2));
+mu = mean(X);
+sigma = std(X);
+X_norm = (X .- mu) ./ sigma;
+
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: First, for each feature dimension, compute the mean
@@ -24,7 +25,7 @@ sigma = zeros(1, size(X, 2));
 %               each feature. 
 %
 % Hint: You might find the 'mean' and 'std' functions useful.
-%       
+%
 
 
 
